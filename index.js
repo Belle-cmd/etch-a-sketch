641af9a6553dbea521d80a31ea4d5b350f52c9ae
BelleProjects <bautistadana55@gmail.com>
2022-08-16 02:44:14 -0600

    Create a basic range slider

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const defaultSize = 16;
-
-let currentSize = defaultSize;
+let currentSize;
 
 const canvas = document.getElementById("canvas");
+const slider = document.getElementById("slider");
+let sliderOutput = document.getElementById("slider-text");
 
+window.onload = (event) => {
+    sliderOutput.textContent = slider.value;  // set slider's text to the slider' default value at start
+}
 
 /**
  * Creates a grid based on parameters. A border is added to each grid cell
@@ -21,6 +24,12 @@ function createGrid(size=16, border) {
 
     // specify grid-template parameters to arrange how the the grids look
     canvas.style.gridTemplate = `repeat(${size}, 20px) / repeat(${size}, 20px)`;
+}
+
+
+// every time the slider is toggled, its value is shown through p tag's text
+slider.oninput = function() {
+    sliderOutput.textContent = this.value;
 }
 
 createGrid(currentSize, true);
